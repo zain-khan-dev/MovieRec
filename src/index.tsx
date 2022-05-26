44725dac837eb4dde2d8059348bca51d80a0ced6
Zain Khan <zainkhanjune@gmail.com>
2022-05-26 22:33:29 +0530

    Added initial directory structure and react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+  <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
